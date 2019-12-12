b26d8f82c19c8abb15ae4f8c76bfd6f525ffa733
slguan <slguan@taosdata.com>
2019-12-12 10:48:18 +0800

    release beta version 1.6.5.0

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,5 +1,5 @@
-char version[64] = "1.6.4.2";
+char version[64] = "1.6.5.0";
 char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "b9a62d60dc1d4a41452a9bc94e3a0924485c3a75";
-char gitinfoOfInternal[128] = "e6445addc77e8c96dcb57221fa6ab5dcde0458f7";
-char buildinfo[512] = "Built by root at 2019-12-10 10:31";
+char gitinfo[128] = "2b09a97f690ffc1f5e1cec91a14d6d13661952d5";
+char gitinfoOfInternal[128] = "0a4e5137653a65912550e43c6e173b8dc0098b87";
+char buildinfo[512] = "Built by ubuntu at 2019-12-12 10:47";
